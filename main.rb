66c4a68b635947f37531cb692684b60188aa609c
okayblue <56899411+okayblue@users.noreply.github.com>
2022-07-20 21:53:36 -0400

    Add code guessing method

--- main.rb
+++ main.rb
@@ -9,10 +9,23 @@ class CodeMaker
 
   def cpu_select_code
     self.code = CODE_CHOICES.sample(4).join
+    puts 'The secret code has been chosen'
   end
 end
 
 class CodeBreaker
+  attr_accessor :guess
+
   def initialize
+    @guess = 0
+  end
+
+  def code_guess
+    guess_input = ''
+    until guess_input.length == 4 && guess_input.split('').all? { |i| ['1', '2', '3', '4', '5', '6'].include?(i) }
+      puts 'Enter a guess - 4 numbers from 1-6'
+      guess_input = gets.chomp
+    end
+    self.guess = guess_input
   end
 end
